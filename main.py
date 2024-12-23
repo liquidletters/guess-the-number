c7ab514b1af08659e924394334006ce6f37ce6fd
liquidletters <ryansiegenthaler6@gmail.com>
2024-12-22 16:17:26 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import random
+"""Because i didnt understand emilys, i made my own lol"""
+
+num = random.randint(1, 10)
+guess = None
+
+while guess != num:
+  guess = input("guess a number between 1 and 100: ")
+  guess = int(guess)
+
+if guess == num:
+  print("congratulations! you won!")
+  break
+else:
+  print("nope, sorry. try again!")
+
